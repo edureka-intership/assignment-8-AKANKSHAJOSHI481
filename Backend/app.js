23c51efae7cc1d9cee3f549f613f85dbc533017f
AKANKSHAJOSHI481 <joshiakanksha481@gmail.com>
2022-07-20 21:31:48 +0530

    assign 8 merge

--- Backend/app.js
+++ Backend/app.js
@@ -2,11 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const restaurant = require('./routes/restaurant')
 const mealtypes = require('./routes/mealtype')
-<<<<<<< HEAD
 const menu = require('./routes/menu')
 const payment = require('./routes/payment')
-=======
->>>>>>> 61667e162d3b97e58f643ab2e39927baaf7314b7
 const mongoose = require('mongoose')
 const cors = require('cors')
 //connect with MongoDB
@@ -26,11 +23,8 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use('/restaurant',restaurant)
 app.use('/mealtypes',mealtypes)
-<<<<<<< HEAD
 app.use('/menu',menu)
 app.use('/pay',payment)
-=======
->>>>>>> 61667e162d3b97e58f643ab2e39927baaf7314b7
 //Try to include MVC
 app.listen(port,()=>{
     console.log('listening on port' ,port);
